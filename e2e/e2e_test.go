1b9b1e020bfb889203aa6153e4f51f1da81d1936
sshota0809 <8736380+sshota0809@users.noreply.github.com>
2022-08-13 15:22:09 +0900

    ci: Add Error pattern test

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -14,7 +14,16 @@ var _ = Describe("e2e tests", func() {
 		time.Sleep(10 * time.Second)
 	})
 
-	It("test step", func() {
+	AfterEach(func() {
+		services, err := clientSet.CoreV1().Services("default").List(context.Background(), metav1.ListOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		for _, service := range services.Items {
+			err = clientSet.CoreV1().Services("default").Delete(context.Background(), service.GetName(), metav1.DeleteOptions{})
+			Expect(err).NotTo(HaveOccurred())
+		}
+	})
+
+	It("available IP exists", func() {
 		svc := &corev1.Service{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:        "svc01",
@@ -36,5 +45,49 @@ var _ = Describe("e2e tests", func() {
 		svc, err := clientSet.CoreV1().Services("default").Create(context.Background(), svc, metav1.CreateOptions{})
 		Expect(err).NotTo(HaveOccurred())
 		Expect(svc.Spec.LoadBalancerIP).Should(Equal("10.10.10.10"))
+	})
+
+	It("available IP doesn't exists", func() {
+		svc := &corev1.Service{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "svc01",
+				Annotations: map[string]string{},
+			},
+			Spec: corev1.ServiceSpec{
+				Type: "LoadBalancer",
+				Selector: map[string]string{
+					"label": "test01",
+				},
+				Ports: []corev1.ServicePort{
+					corev1.ServicePort{
+						Protocol: "TCP",
+						Port:     443,
+					},
+				},
+			},
+		}
+		svc, err := clientSet.CoreV1().Services("default").Create(context.Background(), svc, metav1.CreateOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		svc = &corev1.Service{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "svc02",
+				Annotations: map[string]string{},
+			},
+			Spec: corev1.ServiceSpec{
+				Type: "LoadBalancer",
+				Selector: map[string]string{
+					"label": "test02",
+				},
+				Ports: []corev1.ServicePort{
+					corev1.ServicePort{
+						Protocol: "TCP",
+						Port:     443,
+					},
+				},
+			},
+		}
+		_, err = clientSet.CoreV1().Services("default").Create(context.Background(), svc, metav1.CreateOptions{})
+		Expect(err).To(HaveOccurred())
 	})
 })
